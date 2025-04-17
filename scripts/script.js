e90c5ec4c97fa0ea2387c8e1cfbdd473c39bc962
flines <albr.ofm@gmail.com>
2025-04-17 17:08:10 -0300

    2025 update

--- scripts/script.js
+++ scripts/script.js
@@ -2,13 +2,18 @@ var targetFlizzyImage = document.getElementById("FlizzyTargetImage");
 var targetFlizzy = document.getElementById("flizzytarget");
 
 var fave = true;
+var br;
 
 function flizzy_Disappear() {
-		targetFlizzy.className = "flizzyDisappearingSadIKnowThisIsARealUnpolishedAndStupidWayOfCodingTheDifferentAnimationSpeedsIAmSoSorry";
-      }
+	targetFlizzy.className = "flizzyDisappearingSadIKnowThisIsARealUnpolishedAndStupidWayOfCodingTheDifferentAnimationSpeedsIAmSoSorry";
+}
 	  
 function flizzy_YesOrYes() {
-		targetFlizzy.className = "flizzyDisappearing";
+	targetFlizzy.className = "flizzyDisappearing";
+}
+
+function changeLanguage(condition) {
+	br = condition;
 }
 
 function favoriteFlintastic() {
@@ -16,14 +21,26 @@ function favoriteFlintastic() {
 	var faveStar = document.getElementById("favorited_star_comments");
 	var faveRating = document.getElementById("favorited_ratings_comments");
 	
-	if(!fave) {
+	if(!fave && !br) {
 		faveRating.textContent = '   6,251 favorites';
 		faveText.textContent = 'Unfavorite';
 		faveStar.style.color = '#E8950D';
 		fave = true;
-	} else {
+	} else if (fave && !br) {
 		faveRating.textContent = '   6,250 favorites';
 		faveText.textContent = 'Favorite';
+		faveStar.style.color = '#EEEEEE';
+		fave = false;
+	}
+	
+	if(!fave && br) {
+		faveRating.textContent = '   6,251 curtidas';
+		faveText.textContent = 'Curtido';
+		faveStar.style.color = '#E8950D';
+		fave = true;
+	} else if (fave && br) {
+		faveRating.textContent = '   6,250 curtidas';
+		faveText.textContent = 'Não curtido';
 		faveStar.style.color = '#EEEEEE';
 		fave = false;
 	}
