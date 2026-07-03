6aad05b95ff15aa9988b4ad4b1c4278a34a04393
raflines <37344174+raflines@users.noreply.github.com>
2026-07-03 14:56:39 -0300

    archives and lit stuff

---
+++ legacy-2023-late/scripts/script.js
@@ -0,0 +1,10 @@
+var targetFlizzyImage = document.getElementById("FlizzyTargetImage");
+var targetFlizzy = document.getElementById("flizzytarget");
+
+function flizzy_Disappear() {
+		targetFlizzy.className = "flizzyDisappearingSadIKnowThisIsARealUnpolishedAndStupidWayOfCodingTheDifferentAnimationSpeedsIAmSoSorry";
+      }
+	  
+function flizzy_YesOrYes() {
+		targetFlizzy.className = "flizzyDisappearing";
+}
